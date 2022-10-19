0e522cc11b15fbfa36f496df53fa76ec3005a776
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-19 20:33:21 +0530

    Program added

--- Basic_CoreProgram/Program.cs
+++ Basic_CoreProgram/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Basic Core Psrogram");
-        }
+            Console.WriteLine("Coin Flip Count");
+            double Toss = Convert.ToDouble(Console.ReadLine());
+            int Hcount = 0;
+            int Tcount = 0;
+            for (int i = 0; i < Toss; i++)
+            {
+                Random random = new Random();
+                int coin = random.Next(0, 2);
+                if (coin < .5)
+                {
+                    Hcount += 1;
+                }
+                else
+                    Tcount += 1;
+            }
+            Console.WriteLine("Head count: " + Hcount);
+            Console.WriteLine("Tail count: " + Tcount);
+            double Hpercent = ((double)Hcount / Toss) * 100;
+            Console.WriteLine("Head percentage is: " + Hpercent);
+            double Tpercent = (((double)Tcount / Toss)) * 100;
+            Console.WriteLine("Tail percentage is: " + Tpercent);
+        }   
     }
 }
