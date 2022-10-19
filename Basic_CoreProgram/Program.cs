b73eb134b705f8a7221c033620558340ee65703d
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-19 20:23:36 +0530

    Basic Program

---
+++ Basic_CoreProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Basic_CoreProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Core Psrogram");
+        }
+    }
+}
